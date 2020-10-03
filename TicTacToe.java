31daa50aba24f8b94678393d3b75ff272a003d7a
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-03 14:06:38 +0530

    Added:Welcome Message

---
+++ TicTacToe.java
@@ -0,0 +1,13 @@
+package com.blz.training;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+
+		// Welcome message
+		System.out.println("Welcome to TicTacToe program");
+	}
+
+}
