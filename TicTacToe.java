9ced89d50f527657234fe32b4530b6831024a4ca
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-03 14:08:24 +0530

    Refactor:Created Board

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,10 +4,22 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+	// Create board with Empty Spaces
+	private char[] createBoard() {
+		char[] board = new char[10];
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 
 		// Welcome message
 		System.out.println("Welcome to TicTacToe program");
+
+		TicTacToe cb = new TicTacToe();
+		cb.createBoard();
 	}
 
 }
